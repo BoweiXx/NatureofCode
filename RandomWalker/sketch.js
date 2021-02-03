de4e90e6aab36c0c137a6e9a46b19f01cc02ea3f
BoweiXx <boweixu929@gmail.com>
2021-02-03 17:38:23 +0800

    DOF is always 4, and ix only -1, need to debug

--- RandomWalker/sketch.js
+++ RandomWalker/sketch.js
@@ -1,7 +1,55 @@
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+  createCanvas(400,400);
 }
 
-function draw() {
-  background(220);
+function draw(){
+  background(0);
+}
+
+class walker{
+  constructor(initx, inity, latticel){
+    this.x = initx;
+    this.y = inity;
+    this.latticel = latticel;//each step is 1 lattice unit, so defining lattice length here also defines the number of the lattice
+  }
+  move_record(){
+    var x_num = windowWidth/this.latticel;
+    var y_num = windowHeight/this.latticel;
+    var DOF = 0;
+    //initialize and store the first step
+    var ix = floor(windowWidth/this.x)+1;
+    var iy = floor(windowHeight/this.y)+1;
+    var arr = [[],[]];
+    arr [[ix], [iy]] = true;
+    //calculate DOF
+    switch(arr[[ix+1], [iy+1]] == true){
+      case true: break;
+      case false: DOF++; break;
+    }
+    switch(arr[[ix+1], [iy-1]] == true){
+      case true: break;
+      case false: DOF++; break;
+    }
+    switch(arr[[ix-1], [iy-1]] == true){
+      case true: break;
+      case false: DOF++; break;
+    }
+    switch(arr[[ix-1], [iy-1]] == true){
+      case true: break;
+      case false: DOF++; break;
+    }
+
+  }
+  move(){
+
+  }
+  DOF(){
+    
+  }
+  boundary_detect(){
+
+  }
+
+
+
 }
